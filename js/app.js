71c4c2fd80994f3ec5364abb4b812ee62e2c2844
CodeYourFuture <mahsa2017>
2018-02-16 16:17:45 +0000

    code cleaned a bit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var APPID = "33fb7454eb75c6a062a86f7f0ee2695a";
-var temp;
 var loc;
 var icon;
 var humidity;
@@ -81,121 +80,71 @@ function sendRequest(url) {
             for (var i = 0; i < 8; i ++) {
                minTempFirstDay.push(Math.round(data.list[i].main.temp_min));
             };
-            console.log(minTempFirstDay)
             min_temp_first_day=Math.min(...minTempFirstDay);
-            console.log(min_temp_first_day);
             arrayTempMin.push(min_temp_first_day);
-            console.log(arrayTempMin); //original array
-
             //secondDaymin
             var minTempSecDay=[];
             for (var i = 8; i < 16; i ++) {
                minTempSecDay.push(Math.round(data.list[i].main.temp_min));
             };
-            console.log(minTempSecDay)
             min_temp_Sec_day=Math.min(...minTempSecDay);
-            console.log(min_temp_Sec_day);
             arrayTempMin.push(min_temp_Sec_day);
-            console.log(arrayTempMin); //original array
-
-            //thirdDay
+            //thirdDaymin
             var minTempThirdDay=[];
             for (var i = 16; i < 24; i ++) {
                minTempThirdDay.push(Math.round(data.list[i].main.temp_min));
             };
-            console.log(minTempThirdDay)
             min_temp_Third_day=Math.min(...minTempThirdDay);
-            console.log(min_temp_Third_day);
             arrayTempMin.push(min_temp_Third_day);
-            console.log(arrayTempMin); //original array
-
-            //fourthDay
+            //fourthDaymin
             var minTempFourthDay=[];
             for (var i = 24; i < 32; i ++) {
                minTempFourthDay.push(Math.round(data.list[i].main.temp_min));
             };
-            console.log(minTempFourthDay)
-            min_temp_Fourth_day=Math.min(...minTempFourthDay);
-            console.log(min_temp_Fourth_day);
+            min_temp_Fourth_day=Math.min(...minTempFourthDay);  
             arrayTempMin.push(min_temp_Fourth_day);
-            console.log(arrayTempMin); //original array
-
-            //FifthDay
+            //FifthDaymin
             var minTempFifthDay=[];
             for (var i = 32; i < 40; i ++) {
                minTempFifthDay.push(Math.round(data.list[i].main.temp_min));
             };
-            console.log(minTempFifthDay)
             min_temp_Fifth_day=Math.min(...minTempFifthDay);
-            console.log(min_temp_Fifth_day);
             arrayTempMin.push(min_temp_Fifth_day);
-            console.log(arrayTempMin); //original array
-
-           /* //old version  
-           for (var i = 0; i < data.list.length; i += 8) {
-                arrayTempMin.push(Math.round(data.list[i].main.temp_min));
-            }; */
-
              //FirstDaymax
              var maxTempFirstDay=[];
              for (var i = 0; i < 8; i ++) {
                 maxTempFirstDay.push(Math.round(data.list[i].main.temp_max));
              };
-             console.log(maxTempFirstDay)
              max_temp_first_day=Math.max(...maxTempFirstDay);
-             console.log(max_temp_first_day);
              arrayTempMax.push(max_temp_first_day);
-             console.log(arrayTempMax); //original array
- 
              //secondDaymax
              var maxTempSecDay=[];
              for (var i = 8; i < 16; i ++) {
                 maxTempSecDay.push(Math.round(data.list[i].main.temp_max));
              };
-             console.log(maxTempSecDay)
              max_temp_Sec_day=Math.max(...maxTempSecDay);
-             console.log(max_temp_Sec_day);
              arrayTempMax.push(max_temp_Sec_day);
-             console.log(arrayTempMax); //original array
- 
-             //thirdDaymax
+            //thirdDaymax
              var maxTempThirdDay=[];
              for (var i = 16; i < 24; i ++) {
                 maxTempThirdDay.push(Math.round(data.list[i].main.temp_max));
              };
-             console.log(maxTempThirdDay)
              max_temp_Third_day=Math.max(...maxTempThirdDay);
-             console.log(max_temp_Third_day);
              arrayTempMax.push(max_temp_Third_day);
-             console.log(arrayTempMax); //original array
- 
              //fourthDaymax
              var maxTempFourthDay=[];
              for (var i = 24; i < 32; i ++) {
                 maxTempFourthDay.push(Math.round(data.list[i].main.temp_max));
              };
-             console.log(maxTempFourthDay)
              max_temp_Fourth_day=Math.max(...maxTempFourthDay);
-             console.log(max_temp_Fourth_day);
              arrayTempMax.push(max_temp_Fourth_day);
-             console.log(arrayTempMax); //original array
- 
              //FifthDaymax
              var maxTempFifthDay=[];
              for (var i = 32; i < 40; i ++) {
                 maxTempFifthDay.push(Math.round(data.list[i].main.temp_max));
              };
-             console.log(maxTempFifthDay)
              max_temp_Fifth_day=Math.max(...maxTempFifthDay);
-             console.log(max_temp_Fifth_day);
              arrayTempMax.push(max_temp_Fifth_day);
-             console.log(arrayTempMax); //original array 
-
-            /* //old version 
-              for (var i = 0; i < data.list.length; i += 8) {
-                arrayTempMax.push(Math.round(data.list[i].main.temp_max));
-            }; */
-
             for (var i = 0; i < data.list.length; i += 8) {
                 arrayDate.push(data.list[i].dt);
             };
